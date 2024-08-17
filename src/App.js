2c944f365f79e43dcb50301dda7cff74f61bd228
YuNandarMoe <yunandarmoe31@gmail.com>
2024-08-17 15:29:47 +0900

    project setup and intro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import logo from './logo.svg';
+import logo from  './logo.svg';
+import Another from './Another';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ count, setCount ] = useState(0);
+
+  function decrement() {
+    setCount(prevCount => prevCount - 1);
+  }
+  function increment() {
+    setCount(prevCount => prevCount + 1);
+  }
+  const someStyle = {
+    background: "blue",
+    color: "white",
+    fontSize: "29px",
+    fontWeight: "bold"
+  }
   return (
     <div className="App">
+
       <header className="App-header">
+        <Another name="hello"/>
+        <div>
+          <span>{count}</span>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        { true &&
+          <p style={someStyle}>{ 3 + 2}</p>
+        }
+
         <a
           className="App-link"
           href="https://reactjs.org"
