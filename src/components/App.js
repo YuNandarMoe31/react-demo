fbd841c0e680f4110b461a600b446c8d56a5b396
YuNandarMoe <yunandarmoe31@gmail.com>
2024-08-19 15:30:14 +0900

    format

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import logo from  './logo.svg';
+import logo from '../logo.svg';
 import Another from './Another';
-import './App.css';
+import '../App.css';
 import { useState } from 'react';
 
 function App() {
-  const [ count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Finish react',
+      isComplete: false
+    }
+  ])
 
   function decrement() {
     setCount(prevCount => prevCount - 1);
@@ -22,7 +30,7 @@ function App() {
     <div className="App">
 
       <header className="App-header">
-        <Another name="hello"/>
+        <Another name="hello" />
         <div>
           <span>{count}</span>
           <button onClick={decrement}>-</button>
@@ -32,8 +40,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        { true &&
-          <p style={someStyle}>{ 3 + 2}</p>
+        {true &&
+          <p style={someStyle}>{3 + 2}</p>
         }
 
         <a
